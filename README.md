7b5a8ab6ea9cbc69349ecd2347b35ba6867dbeb4
scottxiong <github.scott.bear@gmail.com>
2018-01-31 16:52:37 +0800

    server

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EM
+EM is short for Express & Mysql
---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+var mysql = require('mysql');
+
+function createCon(){
+  return mysql.createConnection({
+	host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database:'benchmark',
+    port: 3306
+	})
+}
+var conn=createCon();
+
+app.get('/api/find',(req,res)=>{
+	conn.connect();
+	 conn.query('SELECT * from regular_sentences', function(err, result) {
+    if (err) throw err;
+    // res.send('hello world')
+    res.json({code:200,msg:"OK",result})
+    conn.end();
+    }) 
+})
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
